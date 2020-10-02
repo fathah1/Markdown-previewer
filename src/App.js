bed09fb5cef579950d88e8a200ae5a6e99d5c880
Muhammed Fathah <fathah1@gmail.com>
2020-10-02 14:59:55 +0400

    done just fix the looks trying old method

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+let marked = require('marked');
+
+class  App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      markdown:""
+    }  
+    this.handleChange =this.handleChange.bind(this)
+  }
+
+  
+
+  handleChange(markdown){
+    this.setState({markdown});
+  };
+
+
+
+  render(){ 
+    let { markdown } = this.state;
+
+    return (
+    <div>
+      <header>
+          <h1>Mark Down Previewer</h1>
+          <h2>get to know how your mark down looks</h2>  
       </header>
+
+.     <div className="headings">
+      <h3>write your markdown here</h3>
+      <h3>preview will be here</h3>
+      </div>
+      
+      <textArea value={markdown} name="md" id="editor" cols="100" rows="50" placeholder="write markdown here" onChange={(event)=>this.handleChange(event.target.value)}/>
+      
+      <div dangerouslySetInnerHTML= {{__html:marked(markdown)}}>
+  
+      </div>
+
+
     </div>
-  );
+  )
+  }
 }
 
 export default App;
+
